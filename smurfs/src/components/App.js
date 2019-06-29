65e54706547c0bca2a63d47ba30725c80337a2dd
Ben Q <Quackenbush117@gmail.com>
2019-06-28 21:08:36 -0600

    Fixed my errors. I have something displaying now. No smurfs yet, but I have a title

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
+
+import { getSmurfs } from '../actions'
+
+const App = props => {
+  const fetchSmurfs = event => {
+    event.preventdefault();
+    props.getSmurfs();
+
+  };
+  return (
+    <div>
+      <h2>Something witty about Smurfs</h2>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
 /*
  to wire this component up you're going to need a few things.
  I'll let you do this part on your own. 
  Just remember, `how do I `connect` my components to redux?`
  `How do I ensure that my component links the state to props?`
  */
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+
 
 export default App;
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,17 +1,20 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
-import { FETCH_SMURFS_START } from '../actions'
+import { 
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAIL } from '../actions'
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  */
  
  const initialState = {
    smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
+   fetchingSmurfs: false,
+  //  addingSmurf: false
+  //  updatingSmurf: false
+  //  deletingSmurf: false
    error: ''
  }
 
@@ -26,11 +29,25 @@ import { FETCH_SMURFS_START } from '../actions'
 function reducer(state = initialState, action) {
   console.log('reducer', action);
   switch (action.type) {
+    case FETCH_SMURFS_START:
     return {
       ...state,
       fetchingSmurfs: true,
       error: ""
     };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+        error: ''
+
+      }
+    case FETCH_SMURFS_FAIL:
+      return {
+        ...state,
+        error: action.payload
+      }
     default:
       return state;
   }
